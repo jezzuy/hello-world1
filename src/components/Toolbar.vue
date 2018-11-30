5c6ace70b7a0fe9841cae058428dd769fdfc45ff
jezzuy <zanneuy@gmail.com>
2018-11-30 09:27:02 +0800

    Initializing

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-toolbar color="light-blue darken-4" dark fixed>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>CNFM</v-toolbar-title>
+    <v-toolbar-items>
+      <v-btn flat>
+        <v-icon class="mr-2">grade</v-icon>Projects
+      </v-btn>
+    </v-toolbar-items>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat to="/">Home</v-btn>
+      <v-btn flat to="/about">About</v-btn>
+      <v-btn flat to="/register">Register</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+<script>
+export default {
+
+};
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'vuetify/dist/vuetify.min.css'
+import Vuetify from 'vuetify'
+
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+
+Vue.config.productionTip = false
+
+
+Vue.use(Vuetify);
+
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
